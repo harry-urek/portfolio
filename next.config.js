a58325214677c1d0419d3c70c0b07af3b09dd0a6
Hari Om Bhardwaj <hariombhardwaj038@gmail.com>
2025-04-18 10:19:57 +0000

    hhh

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    experimental: {
+        // Add stability for React 18 with Next 15
+        serverComponentsExternalPackages: [],
+    }
 };
 
 module.exports = nextConfig; 
